d8a9f21f877f1340c3e701f065020e88aed27ce2
rstoro <ryanstoro@gmail.com>
2020-03-12 12:54:20 -0400

    Alpha images are now supported!

--- hide_text.py
+++ hide_text.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 
-from sys import argv, exit
 from os import urandom
 from os.path import exists, isdir
 from optparse import OptionParser
@@ -9,22 +8,13 @@ from random import seed, randrange
 from PIL import Image
 
 
-def get_pixel(img, x, y):
-  try:
-    r, g, b = img.getpixel((x, y))
-    return [r, g, b]
-  except ValueError:
-    r, g, b, a = img.getpixel((x, y))
-    return [r, g, b, a]
-
-
 def load_img(img_path):
   if not exists(img_path) or isdir(img_path):
     raise ValueError('Invalid image path.')
 
   img = Image.open(img_path)
-  if img.mode != 'RGB':
-    raise NotImplementedError('Non-RBG images are not yet supported.')
+  #if img.mode != 'RGB':
+  #  raise NotImplementedError('Non-RBG images are not yet supported.')
 
   return img
 
@@ -39,14 +29,14 @@ def encode(img_path, msg):
 
   #encode msg len into r at pixel (0, 0)
   #NOTE: 255 * 255 * 255 = 16,581,375
-  rgba = get_pixel(img, *(0, 0))
+  rgba = [*img.getpixel((0, 0))]
   rgba[0] = msg_len
   enc.putpixel((0, 0), tuple(rgba))
 
   #encode cryptographically secure random 3 byte seed into rgb at pixel (0, 1)
   s = urandom(3)
-  seed(s)
-  rgba = get_pixel(img, *(0, 1))
+  seed(0)
+  rgba = [*img.getpixel((0, 1))]
   rgba[0:3] = s[0:3]
   enc.putpixel((0, 1), tuple(rgba))
 
@@ -56,7 +46,7 @@ def encode(img_path, msg):
     while cur_xy in [(0, 0), (0, 1)]:
       cur_xy = (randrange(w), randrange(h))
 
-    rgba = get_pixel(img, *cur_xy)
+    rgba = [*img.getpixel(cur_xy)]
     rgba[0] = ord(msg[i]) 
     enc.putpixel(cur_xy, tuple(rgba))
 
@@ -66,13 +56,17 @@ def encode(img_path, msg):
 def decode(img_path):
   img = load_img(img_path)
   w, h = img.size
-
-  msg_len = get_pixel(img, *(0, 0))[0]
-  rgba = get_pixel(img, *(0, 1))
+  msg_len = [*img.getpixel((0, 0))][0]
+  rgba = [*img.getpixel((0, 1))]
   s = rgba[0].to_bytes(1, 'big') + rgba[1].to_bytes(1, 'big') + rgba[2].to_bytes(1, 'big')
-  seed(s)
+  seed(0)
 
-  return ''.join(chr(get_pixel(img, *(randrange(w), randrange(h)))[0]) for _ in range(msg_len))
+  for _ in range(msg_len):
+    cur_xy = (randrange(w), randrange(h))
+    rgba = [*img.getpixel(cur_xy)]
+
+  seed(0)
+  return ''.join(chr(img.getpixel((randrange(w), randrange(h)))[0]) for _ in range(msg_len))
 
 
 if __name__ == '__main__':
